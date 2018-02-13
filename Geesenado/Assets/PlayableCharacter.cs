1e6adf19a44f9a69f6d8efae60c4a707ac2a51d4
Connor Mulqueen <Connor.Mulqueen@gmail.com>
2018-02-13 18:06:58 -0500

    Initial Unity Commit

---
+++ Geesenado/Assets/PlayableCharacter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayableCharacter : Character {
+
+    new void Start()
+    {
+        base.Start();
+    }
+
+    new void Update()
+    {
+        movement();
+    }
+
+    new public void movement()
+    {
+        if (Input.GetKey(KeyCode.W)) //sample movement
+        {
+            _rb.AddForce(Vector2.up * _movementSpeed);
+        }
+    }
+}
