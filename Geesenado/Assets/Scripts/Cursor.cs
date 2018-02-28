e372768c57ab59a8d330b8335266507b9dc4e650
jaredgav <jaredgav@buffalo.edu>
2018-02-28 14:55:29 -0500

    Update pencil lifetime. Added Cursor script, object, and sprite
    Cursor does not follow as desired. Will attempt to work on lab computers to ensure Unity is doing what is expected.

---
+++ Geesenado/Assets/Scripts/Cursor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+    // Scene Objects
+    public Rigidbody2D cursorBody;
+    public Rigidbody2D playerBody;
+
+    // Class Objects
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 mousePosition;
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 vectorFromPlayer = cursorBody.position - playerBody.position;
+        float distanceFromPlayer = vectorFromPlayer.magnitude;
+        cursorBody.position = new Vector2(mousePosition.x - playerBody.position.x, mousePosition.y - playerBody.position.y);
+
+    }
+}
