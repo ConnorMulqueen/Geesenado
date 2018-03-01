d196a482c63cb3b917868149a097cc85773e0971
jaredgav <jaredgav@buffalo.edu>
2018-02-28 21:32:16 -0500

    Cursor obj renamed to Reticle. Reticle works as desired.
    Weapons that have different distances can change reticle distance on the public float for weapon fire distance (currently defaulted to paper shot length). Use this only for ranged weapons.

---
+++ Geesenado/Assets/Scripts/Reticle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle : MonoBehaviour {
+
+    // Scene Objects
+    public Rigidbody2D reticleBody;
+    public Rigidbody2D playerBody;
+
+    // Class Objects
+    public float weaponFireDistance = 7.3f; // Default is paper shot length
+
+    // Use this for initialization
+    void Start()
+    {
+        this.GetComponent<DistanceJoint2D>().distance = weaponFireDistance;
+        reticleBody.position = playerBody.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        float distanceFromPlayer = (playerBody.position - mousePosition).magnitude;
+
+        if(distanceFromPlayer >= weaponFireDistance)
+        {
+            // Enable Fixed Joint, set velocity toward mouse
+            this.GetComponent<DistanceJoint2D>().enabled = true;
+            Vector2 directionToMouse = new Vector2( mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+            reticleBody.velocity = directionToMouse * 10;
+        }
+        else
+        {
+            this.GetComponent<DistanceJoint2D>().enabled = false;
+            reticleBody.transform.position = mousePosition;
+            reticleBody.velocity = new Vector2(0, 0);
+        }
+    }
+}
