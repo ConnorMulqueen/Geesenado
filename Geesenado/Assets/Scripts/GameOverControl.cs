ca5dca1f1e0df01e3845d74a5218e918b3dd8343
jaredgav <jaredgav@buffalo.edu>
2018-03-28 15:12:05 -0400

    Bug fix
    Took out reticle (distance joint caused problems)
    NPC still phase
    Character select needs rework
    Need to create some GameMechanic script to handle winning and all

---
+++ Geesenado/Assets/Scripts/GameOverControl.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverControl : MonoBehaviour {
+
+    private NPC[] myNPCList;
+    // Use this for initialization
+    void Start () {
+        myNPCList = (NPC[]) FindObjectsOfType(typeof(NPC));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        myNPCList = (NPC[])FindObjectsOfType(typeof(NPC));
+        Debug.Log("Num NPCs: " + myNPCList.Length.ToString() );
+        if(myNPCList.Length <= 0)
+        {
+            SceneManager.LoadScene("winScreen");
+        }
+	}
+}
