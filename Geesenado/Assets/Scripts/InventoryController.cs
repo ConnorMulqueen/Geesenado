d4d3dac3bdaa4b2146bc6fab735e600f57a5b837
hjames960 <hjames960@gmail.com>
2018-04-10 18:22:47 -0400

    inventory and health bar
    implement inventory and new health bar system

---
+++ Geesenado/Assets/Scripts/InventoryController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class Inventory : MonoBehaviour {
+	public IHoldable[] inventory;
+	public GameObject playerObject;
+
+
+	void Start () {
+		inventory = new IHoldable[6];
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("Alpha 1")) {
+			if (inventory [1]!= (null)) {
+				inventory [0] = inventory [1];
+			}
+		
+		}
+		if (Input.GetKeyDown ("Alpha 2")) {
+			if (inventory [2]!= (null)) {
+				inventory [0] = inventory [2];
+			}
+
+		}
+		if (Input.GetKeyDown ("Alpha 3")) {
+			if (inventory [3]!= (null)) {
+				inventory [0] = inventory [3];
+			}
+
+		}
+		if (Input.GetKeyDown ("Alpha 4")) {
+			if (inventory [4]!= (null)) {
+				inventory [0] = inventory [4];
+			}
+
+		}
+		if (Input.GetKeyDown ("Alpha 5")) {
+			if (inventory [5]!= (null)) {
+				inventory [0] = inventory [5];
+			}
+
+		}
+		if (Input.GetMouseButton (0)) {
+			if (inventory [0] is IWeapon) {
+				(IWeapon)inventory [0].Fire ();
+			}
+		}
+	}
+	int addItem(IHoldable item){
+		int counter = 0;
+		if (inventory.Contains (item)) {
+			return 0;
+		} 
+		else {
+			for (int i = 1; i < inventory.Length; i++) {
+				if (inventory [i] = null) {
+					inventory [i] = item;
+					break;
+				} else {
+					if(counter==inventory.Length-1){
+						ReplaceInventory (item);
+					}
+					else{
+						counter++;
+					}
+				}
+			}
+			return 1;
+		}
+		
+	}
+	int ReplaceInventory( IHoldable item ){
+		IHoldable repalce = inventory [0];
+		for (int i = 1; i < inventory.Length; i++) {
+			if (inventory [i].Equals (repalce)) {
+				inventory [i] = item;
+				inventory [0] = item;
+			}
+			
+		}
+	
+	}
+
+
+}
--- Geesenado/Assets/Scripts/PlayableCharacter.cs
+++ Geesenado/Assets/Scripts/PlayableCharacter.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using Assets.Scripts;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class PlayableCharacter : Character {
+	public IHoldable[] inventory;
     public Pencil pencil;
     public Paper paper;
+	public float maxHealth=4.0f;
+	public float currentHealth;
+	public Slider healthbar;
 
-    
     new void Start()
     {
+		inventory = new IHoldable[6];
         base.Start();
         Debug.Log("The Player's chosen char is " + PlayerPrefs.GetInt("CharacterSelected"));
 
@@ -28,21 +33,61 @@ public class PlayableCharacter : Character {
         }
         GetComponent<Renderer>().material.SetColor("_EmissionColor", myColor);
 
-
+		healthbar.value = maxHealth;
     }
 
     new void Update()
     {
         movement();
+		currentHealth = base.getHealth();
+		healthbar.value = calcHealth();
+		if (Input.GetKeyDown ("Alpha 1")) {
+			if (inventory [1]!= (null)) {
+				inventory [0] = inventory [1];
+			}
+
+		}
+		if (Input.GetKeyDown ("Alpha 2")) {
+			if (inventory [2]!= (null)) {
+				inventory [0] = inventory [2];
+			}
+
+		}
+		if (Input.GetKeyDown ("Alpha 3")) {
+			if (inventory [3]!= (null)) {
+				inventory [0] = inventory [3];
+			}
+
+		}
+		if (Input.GetKeyDown ("Alpha 4")) {
+			if (inventory [4]!= (null)) {
+				inventory [0] = inventory [4];
+			}
+
+		}
+		if (Input.GetKeyDown ("Alpha 5")) {
+			if (inventory [5]!= (null)) {
+				inventory [0] = inventory [5];
+			}
+
+		}
+		if (Input.GetMouseButton (0)) {
+			if (inventory [0] is IWeapon) {
+				(IWeapon)inventory [0].Fire ();
+			}
+		}
+
     }
 
     new public void movement()
     {
         
     }
+	float calcHealth(){
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
+		return currentHealth / maxHealth;
+	}
+    private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "NPCWeaponTag")
         {
             float dmg = collision.gameObject.GetComponent<IDealsDamage>().DealDamage;
@@ -53,5 +98,42 @@ public class PlayableCharacter : Character {
                 SceneManager.LoadScene("gameOver");
             }
         }
+		if (collision.gameObject.tag == "Geesenado") {
+			_health -= 0.1f;
+		}
     }
+	int addItem(IHoldable item){
+		int counter = 0;
+		if (inventory.Contains (item)) {
+			return 0;
+		} 
+		else {
+			for (int i = 1; i < inventory.Length; i++) {
+				if (inventory [i] = null) {
+					inventory [i] = item;
+					break;
+				} else {
+					if(counter==inventory.Length-1){
+						ReplaceInventory (item);
+					}
+					else{
+						counter++;
+					}
+				}
+			}
+			return 1;
+		}
+
+	}
+	int ReplaceInventory( IHoldable item ){
+		IHoldable repalce = inventory [0];
+		for (int i = 1; i < inventory.Length; i++) {
+			if (inventory [i].Equals (repalce)) {
+				inventory [i] = item;
+				inventory [0] = item;
+			}
+
+		}
+
+	}
 }
