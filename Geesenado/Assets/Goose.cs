729d49a1e289fe49b5af42009639e9d5c5d5e331
jaredgav <jaredgav@buffalo.edu>
2018-04-13 15:17:09 -0400

    UI Updates

--- Geesenado/Assets/Goose.cs
+++ Geesenado/Assets/Goose.cs
@@ -13,13 +13,7 @@ public class Goose : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Vector2 center = geesenado.GetComponent<CircleCollider2D>().offset;
-        //transform.LookAt(center);
-        //transform.Rotate(new Vector3(0, -90, 0), Space.Self);
-        //GetComponent<Rigidbody2D>().position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), center, 3 * Time.deltaTime);
-        GetComponent<DistanceJoint2D>().connectedBody = geesenado.GetComponent<Rigidbody2D>();
-        GetComponent<DistanceJoint2D>().connectedAnchor = geesenado.GetComponent<CircleCollider2D>().offset;
         GetComponent<DistanceJoint2D>().distance = geesenado.GetComponent<CircleCollider2D>().radius;
-
+        Debug.Log("Dis: " + GetComponent<DistanceJoint2D>().distance.ToString());
     }
 }
